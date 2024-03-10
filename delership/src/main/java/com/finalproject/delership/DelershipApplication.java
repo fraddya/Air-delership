edd4b0219ebda75d9ad1ce63605877390c723cc0
fraddy <ssisithasiranga@gmail.com>
2024-03-10 15:26:01 +0530

    first setup

--- delership/src/main/java/com/finalproject/delership/DelershipApplication.java
+++ delership/src/main/java/com/finalproject/delership/DelershipApplication.java
@@ -2,8 +2,16 @@ package com.finalproject.delership;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableDiscoveryClient
+@EnableTransactionManagement
+@EnableFeignClients
 public class DelershipApplication {
 
 	public static void main(String[] args) {
