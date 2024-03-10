082ab02483f938da1bf17b8acd81be20b9a585f9
fraddy <ssisithasiranga@gmail.com>
2024-03-10 15:11:03 +0530

    first setup

---
+++ delership/src/main/java/com/finalproject/delership/DelershipApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.delership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelershipApplication.class, args);
+	}
+
+}
