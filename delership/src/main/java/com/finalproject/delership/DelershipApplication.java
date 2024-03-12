bffa46df3ee75c38c7d3d80aa3470ec94bb7facf
fraddy <ssisithasiranga@gmail.com>
2024-03-12 23:07:09 +0530

    jj

--- delership/src/main/java/com/finalproject/delership/DelershipApplication.java
+++ delership/src/main/java/com/finalproject/delership/DelershipApplication.java
@@ -2,14 +2,12 @@ package com.finalproject.delership;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = "com.finalproject.*")
 @EnableJpaAuditing
-@EnableDiscoveryClient
 @EnableTransactionManagement
 @EnableFeignClients
 public class DelershipApplication {
